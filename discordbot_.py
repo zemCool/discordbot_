68041e43e868d47044a8638c45e099c7ffc650c9
zemCool <73570667+zemCool@users.noreply.github.com>
2020-12-17 02:26:13 +0600

    Added new functions such as "join", "disconnect" and chat filterer.

--- discordbot_.py
+++ discordbot_.py
@@ -44,4 +44,48 @@ async def hello(ctx):
     await ctx.send(f"{author}, Hello! {random.choice(random_greetings)}")
 
 
+# Chat filter
+@client.event
+async def on_message(message):
+    await client.process_commands(message)
+    msg = message.content.lower()
+    if msg in bad_language:
+        await message.delete()
+
+
+# Disconnect from a voice channel
+@client.command(name='disconnect_', help='bot disconnects from the voice channel.', aliases=['disconnect'])
+async def disconnect_(ctx):
+    await ctx.channel.purge(limit=1)
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.disconnect()
+        await ctx.send(f'Bot is disconnected from: {channel}')
+    else:
+        voice = await channel.connect
+
+
+# Join to a voice channel
+@client.command(name='join', help='bot joins to voice channel.')
+async def join(ctx):
+    await ctx.channel.purge(limit=1)
+    global voice
+    channel = ctx.message.author.voice.channel
+    voice = get(client.voice_clients, guild=ctx.guild)
+
+    if voice and voice.is_connected():
+        await voice.move_to(channel)
+    else:
+        voice = await channel.connect()
+        await ctx.send(f'Bot joined to: {channel}')
+
+
+# An 8ball game
+@client.command(aliases=['8ball'])
+async def _8ball(ctx, *, question):
+    await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
+
 client.run('token')
